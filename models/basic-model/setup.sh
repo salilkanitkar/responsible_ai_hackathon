1e76c3565c64c5c660a40369e53fed19c9381fa3
pk <praveenkrishna@praveens-MacBook-Pro.local>
2020-05-06 21:50:15 -0700

    Adding balancing of classes using smote

--- models/basic-model/setup.sh
+++ models/basic-model/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 conda install -y pandas
-pip install -U -q pip tensorflow==2.1 git+https://github.com/tensorflow/docs numpy chakin scikit-learn
+pip install -U -q pip tensorflow==2.1 git+https://github.com/tensorflow/docs numpy chakin scikit-learn imblearn
